390aabcd64ff6fe5e60680b50df15b0600f2c392
LucLev <luckylevillain@gmail.com>
2019-04-11 17:45:45 +0200

    ajout de l'exercice 4 routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,22 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+
+const routes: Routes = [
+  { path: '', component: UserProfileComponent },
+  { path: 'signup', component: SignUpComponent },
+  { path: 'user', component: UserProfileComponent },
+  { path: 'movies', component: MoviesComponent },
+];
+
 
 import { AppComponent } from './app.component';
 import { SignUpComponent } from './sign-up/sign-up.component';
 import { UserProfileComponent } from './user-profile/user-profile.component';
 import { MoviesComponent } from './movies/movies.component';
 import { HighlightDirective } from './highlight.directive';
+import { MenuComponent } from './menu/menu.component';
 
 @NgModule({
   declarations: [
@@ -14,13 +24,18 @@ import { HighlightDirective } from './highlight.directive';
     SignUpComponent,
     UserProfileComponent,
     MoviesComponent,
-    HighlightDirective
+    HighlightDirective,
+    MenuComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
+    [RouterModule.forRoot(routes)],
   ],
+
+  exports: [RouterModule],
   providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
+export class AppRoutingModule {}
